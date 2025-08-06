755a893247d5a1917fc719be5fc5cc99d2f74800
Collier-A-B <benjaminac46@gmail.com>
2025-08-06 16:03:39 -0400

    move scoreboard display function

---
+++ score_board.cpp
@@ -0,0 +1,16 @@
+#include "score_board.hpp"
+#include "player.hpp"
+#include <iostream>
+
+static void displayScoreBoard() {
+    int p1Score = Player::getPlayer1()->getScore();
+    int p2Score = Player::getPlayer2()->getScore();
+
+    std::string p1Name = Player::getPlayer1()->getName();
+    std::string p2Name = Player::getPlayer2()->getName();
+    std::cout << "**********************************************************" << std::endl;
+    std::cout << "\t\tScore Board" << std::endl;
+    std::cout << "\t" << p1Name << " Score: " << p1Score << std::endl;
+    std::cout << "\t" << p2Name << " Score: " << p2Score << std::endl;
+    std::cout << "**********************************************************" << std::endl;
+}
--- score_board.hpp
+++ score_board.hpp
@@ -9,16 +9,5 @@ class score_board {
         score_board(const score_board&) = delete; // Prevent copy-construction
         score_board& operator=(const score_board&) = delete; // Prevent assignment 
 
-        static void displayScoreBoard() {
-            int p1Score = Player::getPlayer1()->getScore();
-            int p2Score = Player::getPlayer2()->getScore();
-
-            std::string p1Name = Player::getPlayer1()->getName();
-            std::string p2Name = Player::getPlayer2()->getName();
-            std::cout << "**********************************************************" << std::endl;
-            std::cout << "\t\tScore Board" << std::endl;
-            std::cout << "\t" << p1Name << " Score: " << p1Score << std::endl;
-            std::cout << "\t" << p2Name << " Score: " << p2Score << std::endl;
-            std::cout << "**********************************************************" << std::endl;
-        }
+        static void displayScoreBoard();
 };
